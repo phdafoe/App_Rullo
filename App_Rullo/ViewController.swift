b74c41d53955a26cef3890c2a845f9fd501924e5
phdafoe <andresocampo@MacBook-Pro-de-Andres.local>
2017-03-15 11:55:08 +0100

    Initial Commit

---
+++ App_Rullo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App_Rullo
+//
+//  Created by Andres Ocampo on 15/3/17.
+//  Copyright © 2017 Andres Ocampo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
